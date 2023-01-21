6b0ea13a837615d50a0e4772aa45dd97f2815f3b
slavekrouta <97255074+slavekrouta@users.noreply.github.com>
2023-01-21 22:02:04 +0100

    Update script.php

--- script.php
+++ script.php
@@ -1,23 +1,25 @@
 <?php
-$date = date("d-m-Y");
+$date = date("d-m-Y_h_i");
 $new_file_name = "copy-" . $date . ".html";
 if(copy("index.html", $new_file_name)) {
 } else {
 }
 if(isset($_POST['message']) && !empty($_POST['message'])) {
+    echo "Ukládání...";
     if(!empty($_POST['usrname'])) {
         $usrname = $_POST['usrname'];
         $message = $_POST['message'];
-        $time = date("Y-m-d h:i:s");
+        $time = date("Y-m-d");
         $text = "<p>" . $usrname . ": " . $message . " v čase " . $time . "</p>";
         file_put_contents("index.html", $text, FILE_APPEND);
     }
     else{
         $message = $_POST['message'];
-        $time = date("Y-m-d h:i:s");
+        $time = date("Y-m-d");
         $text = "<p>" . "Anonymious: " . $message . " v čase " . $time . "</p>";
         file_put_contents("index.html", $text, FILE_APPEND);
     }
+    echo " Hotovo";
 }
 else{
     echo "Zpráva je prázdná!";
